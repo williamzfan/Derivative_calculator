e94e81d6dedb7d141d2ee86146273ba933892cc3
williamzfan <35201562+williamzfan@users.noreply.github.com>
2018-01-20 18:55:24 -0500

    Update derivative.R

--- derivative.R
+++ derivative.R
@@ -1,8 +1,8 @@
 
-f <- function(x) (4*x^3 + 3 * x^2 - 2 * x - 1)
+f <- function(x) (4*x^3 + 3*x^2 + 2*x + 1)
 
 df <- function(x) {}
 body(df) <- D(body(f), 'x')
 
-curve(f, -4, 4, ylab = "Function and Derivative")
-curve(df, -4, 4, add = TRUE, col = "Red")
+curve(f , -10, 10, ylab = "Function and Derivative")
+curve(df, -10, 10, add = TRUE, col = "Red")
